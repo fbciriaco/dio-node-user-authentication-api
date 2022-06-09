d46043f9009be0c2330dacd05b01df9fd2f60e13
fbciriaco <fb.ciriaco@gmail.com>
2022-06-09 11:51:12 -0300

    Add Admin and Pass on Db

--- live-01/sql/init.sql
+++ live-01/sql/init.sql
@@ -1,4 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE IF NOT EXISTS app_user(
     uuid uuid DEFAULT uuid_generate_v4(),
@@ -6,3 +7,5 @@ CREATE TABLE IF NOT EXISTS app_user(
     password VARCHAR NOT NULL,
     PRIMARY KEY (uuid)
 )
+
+INSERT INTO app_user(username, password) VALUES ('admin', crypt('admin', 'my_salt'));
